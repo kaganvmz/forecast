405cc963c9e932f472b9f20391ee976414f2b28d
Slava Shevchenko <kaganvmz@gmail.com>
2017-11-26 22:43:40 +0200

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//CSS
+import './css/style.css';
+
+const element = <h1>Hell, world</h1>;
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
